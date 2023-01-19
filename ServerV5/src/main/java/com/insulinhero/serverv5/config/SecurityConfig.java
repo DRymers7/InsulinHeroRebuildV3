1e9ebefefcf630ff5239260acb60448a8c0e6b90
Derek Rymers <rymersd@gmail.com>
2023-01-19 11:00:28 -0500

    Security sucks

---
+++ ServerV5/src/main/java/com/insulinhero/serverv5/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.insulinhero.serverv5.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
