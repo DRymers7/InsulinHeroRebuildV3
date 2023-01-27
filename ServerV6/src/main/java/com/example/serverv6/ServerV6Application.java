8d4f2f33ef0d2a2f437951d3a81546d1c9a97dd2
Derek Rymers <rymersd@gmail.com>
2023-01-27 12:59:11 -0500

    Initial commit

---
+++ ServerV6/src/main/java/com/example/serverv6/ServerV6Application.java
@@ -0,0 +1,13 @@
+package com.example.serverv6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerV6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerV6Application.class, args);
+	}
+
+}
---
+++ ServerV6/src/test/java/com/example/serverv6/ServerV6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverv6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerV6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
