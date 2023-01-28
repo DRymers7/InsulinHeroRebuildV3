09f6569b7acc5623fd00cb3324c10975dbe33fa3
Derek Rymers <rymersd@gmail.com>
2023-01-28 12:57:06 -0500

    Fixing issues with original script

--- database/user.sql
+++ database/user.sql
@@ -2,24 +2,24 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER final_capstone_owner
-WITH PASSWORD 'finalcapstone';
+CREATE USER insulin_hero_rebuild_db_owner
+WITH PASSWORD 'insulinhero';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_owner;
+TO insulin_hero_rebuild_db_owner;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_owner;
+TO insulin_hero_rebuild_db_owner;
 
-CREATE USER final_capstone_appuser
-WITH PASSWORD 'finalcapstone';
+CREATE USER insulin_hero_rebuild_db_appuser
+WITH PASSWORD 'insulinhero';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_appuser;
+TO insulin_hero_rebuild_db_appuser;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_appuser;
+TO insulin_hero_rebuild_db_appuser;
